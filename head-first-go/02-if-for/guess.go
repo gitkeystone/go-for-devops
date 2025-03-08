0848a4be8fadd2f398f692480ceb6cde9fb375c7
Chen Xiaohui <ysucxh@163.com>
2025-03-08 23:45:37 +0800

    02-1

--- head-first-go/02-if-for/guess.go
+++ head-first-go/02-if-for/guess.go
@@ -1,9 +1,15 @@
+// guess challenges players to guess a random number.
 package main
 
 import (
 	"fmt"
 	"math/rand"
 	// "time"
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,5 +20,38 @@ func main() {
 	target := rand.Intn(100) + 1 // Intn(N) -> [0, N)
 	fmt.Println("I've chosen a random number between 1 and 100.")
 	fmt.Println("Can you guess it?")
-	fmt.Println(target)
+	// fmt.Println(target)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+
+		fmt.Println("Make a guess:")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
+	}
+
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was:", target)
+	}
+
 }
