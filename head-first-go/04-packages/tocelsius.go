734fe835cedd507c3293cd9a5e81f13fbceb2660
Chen Xiaohui <ysucxh@163.com>
2025-03-10 17:56:41 +0800

    04

---
+++ head-first-go/04-packages/tocelsius.go
@@ -0,0 +1,38 @@
+// tocelsius converts a temperature from Fahrenheit to Celsius.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n') // input 包含回车符
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64) // err 赋值
+	if err != nil {
+		return 0, err
+	}
+
+	return number, nil
+}
+
+func main() {
+	fmt.Print("Enter a temperature in Fahrenheit: ")
+	fahrenheit, err := getFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	celsius := (fahrenheit - 32) * 5 / 9
+	fmt.Printf("%0.2f degrees Celsius\n", celsius)
+}
