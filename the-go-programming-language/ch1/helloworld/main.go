64e82a00a96733ebde7155d77915268bbb63b7ea
Chen Xiaohui <percxh@gmail.com>
2025-12-03 08:30:14 +0800

    echo4/main.go

--- the-go-programming-language/ch1/helloworld/main.go
+++ the-go-programming-language/ch1/helloworld/main.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, 世界")
+	var a, b, _ = test()
+	fmt.Println(a, b)
+
+	for i := range []int{1, 2, 3} {
+		fmt.Println(i)
+	}
+}
+
+func test() (int, string, error) {
+	return 1, "", nil
 }
