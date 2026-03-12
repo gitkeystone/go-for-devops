094e689d6720445d8d3dec7de85f90b99a714303
Chen Xiaohui <percxh@gmail.com>
2026-03-12 23:48:19 +0800

    1. 开尔文温度

--- the-go-programming-language/ch2/tempconv0/conv.go
+++ the-go-programming-language/ch2/tempconv0/conv.go
@@ -1,9 +1,21 @@
 package tempconv
 
+// CToF 把摄氏温度转化为华氏温度
 func CToF(c Celsius) Fahrenheit {
 	return Fahrenheit(c*9/5 + 32)
 }
 
+// CToK 把摄氏温度转化为开尔文温度
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c + 273.15)
+}
+
+// FToC 把华氏温度转化为摄氏温度
 func FToC(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
+}
+
+// KToC 把开尔文温度转化为摄氏温度
+func KToC(k Kelvin) Celsius {
+	return Celsius(k - 273.15)
 }
--- the-go-programming-language/ch2/tempconv0/tempconv.go
+++ the-go-programming-language/ch2/tempconv0/tempconv.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
@@ -18,3 +19,5 @@ func (c Celsius) String() string {
 func (f Fahrenheit) String() string {
 	return fmt.Sprintf("%g℉", f)
 }
+
+func (k Kelvin) String() string { return fmt.Sprintf("%gK", k) }
